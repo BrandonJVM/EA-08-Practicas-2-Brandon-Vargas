8cb552833181d1d84d4bc5008c11cce93e9d495d
BrandonJVM <bvargas40854@ufide.ac.cr>
2024-10-08 23:37:58 -0600

    Add files via upload

---
+++ BrandonVargasMoreira_EA_08_Practicas_2/src/main/java/BrandonVargasMoreira_EA_08_Practicas_2/BrandonVargasMoreiraEa08Practicas2Application.java
@@ -0,0 +1,13 @@
+package BrandonVargasMoreira_EA_08_Practicas_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrandonVargasMoreiraEa08Practicas2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrandonVargasMoreiraEa08Practicas2Application.class, args);
+	}
+
+}
---
+++ BrandonVargasMoreira_EA_08_Practicas_2/src/test/java/BrandonVargasMoreira_EA_08_Practicas_2/BrandonVargasMoreiraEa08Practicas2ApplicationTests.java
@@ -0,0 +1,13 @@
+package BrandonVargasMoreira_EA_08_Practicas_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrandonVargasMoreiraEa08Practicas2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
